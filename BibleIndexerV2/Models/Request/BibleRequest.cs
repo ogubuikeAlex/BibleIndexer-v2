f9111914be0d5d13220d94621d14a3ac61738c99
Alexandra ogubuike <ogubuikealex@gmail.com>
2023-03-04 18:34:06 +0100

    feat: add request DTOs

---
+++ BibleIndexerV2/Models/Request/BibleRequest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BibleIndexerV2.Models.Request
+{
+    internal class BibleRequest
+    {
+    }
+
+    public class GetBibleVerseRequest
+    {
+        public int VerseNumber { get; set; }
+        public string BookNameInFull { get; set; }
+        public int ChapterNumber { get; set; }
+    }
+}
