f88d55a6573029e2a05ebe6ca7aa6e5d549bd8c5
Alexandra ogubuike <ogubuikealex@gmail.com>
2023-03-04 19:07:23 +0100

    test: add test for the Get all books of the bible method

---
+++ BibleIndexerTest/GetAllBooksOfTheBibleTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace BibleIndexerTest
+{
+    public class GetAllBooksOfTheBibleTest
+    {
+        public class BibleApiTests
+        {
+            [Fact]
+            public async Task GetAllBooksOfTheBible_ShouldReturnListOfBooks()
+            {
+                // Arrange
+                var expectedBookCount = 66; // The Bible has 66 books
+
+                // Act
+                var books = await BibleApi.GetAllBooksOfTheBible();
+
+                // Assert
+                Assert.NotNull(books);
+                Assert.IsType<List<object>>(books);
+
+                var bookList = books as List<object>;
+                Assert.Equal(expectedBookCount, bookList.Count);
+
+                var firstBook = bookList.FirstOrDefault();
+                Assert.NotNull(firstBook);
+                Assert.True(firstBook.GetType().GetProperty("Id") != null);
+                Assert.True(firstBook.GetType().GetProperty("Book") != null);
+            }
+        }
+
+    }
+}
