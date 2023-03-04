b13c7e9d822ca8e906af07c41dfd4180cd876354
Alexandra ogubuike <ogubuikealex@gmail.com>
2023-03-04 19:07:42 +0100

    test: add test for get all chapters of the bible method

---
+++ BibleIndexerTest/GetAllChaptersOfTheBibleTest.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace BibleIndexerTest
+{
+    public class GetAllChaptersOfTheBibleTest
+    {
+        public class BibleApiTests
+        {
+            [Fact]
+            public async Task GetAllVersesInAChapterOFTheBible_ShouldReturnListOfVerses()
+            {
+                // Arrange
+                var expectedVersesCount = 31; // Genesis 1 has 31 verses
+
+                var request = new GetBibleVerseRequest
+                {
+                    BookNameInFull = "Genesis",
+                    ChapterNumber = 1,
+                };
+
+                // Act
+                var response = await BibleApi.GetAllVersesInAChapterOFTheBible(request);
+
+                // Assert
+                Assert.NotNull(response);
+                Assert.IsType<VersesResponse>(response);
+                Assert.Equal(request.BookNameInFull, response.BookName);
+
+                var versesDropdown = response.DropDown as List<dynamic>;
+                Assert.NotNull(versesDropdown);
+                Assert.Equal(expectedVersesCount, versesDropdown.Count);
+
+                var firstVerse = versesDropdown.FirstOrDefault();
+                Assert.NotNull(firstVerse);
+                Assert.True(firstVerse.GetType().GetProperty("Id") != null);
+            }
+
+            [Fact]
+            public async Task GetAllVersesInAChapterOFTheBible_ShouldReturnNull_WhenInvalidChapterNumber()
+            {
+                // Arrange
+                var request = new GetBibleVerseRequest
+                {
+                    BookNameInFull = "Genesis",
+                    ChapterNumber = 100,
+                };
+
+                // Act
+                var response = await BibleApi.GetAllVersesInAChapterOFTheBible(request);
+
+                // Assert
+                Assert.Null(response);
+            }
+
+            [Fact]
+            public async Task GetAllVersesInAChapterOFTheBible_ShouldReturnNull_WhenInvalidBookName()
+            {
+                // Arrange
+                var request = new GetBibleVerseRequest
+                {
+                    BookNameInFull = "InvalidBook",
+                    ChapterNumber = 1,
+                };
+
+                // Act
+                var response = await BibleApi.GetAllVersesInAChapterOFTheBible(request);
+
+                // Assert
+                Assert.Null(response);
+            }
+        }
+
+    }
+}
