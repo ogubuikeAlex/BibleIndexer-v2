a80fc3862897f4ea01df959dbc88c018e91e9a38
Alexandra ogubuike <ogubuikealex@gmail.com>
2023-03-04 18:34:54 +0100

    feat: add interface for implementation and unit testing

---
+++ BibleIndexerV2/Services/Interfaces/IBibleService.cs
@@ -0,0 +1,18 @@
+﻿using BibleIndexerV2.Models.Request;
+using BibleIndexerV2.Models.Response;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BibleIndexerV2.Services.Interfaces
+{
+    public interface IBibleService
+    {
+        Task<BibleVerseResponse?> GenerateRandomBibleVerse();
+        Task<object> GetAllBooksOfTheBible();
+        Task<VersesResponse?> GetAllVersesInAChapterOFTheBible(GetBibleVerseRequest request);
+        Task<BibleVerseResponse?> GetBibleVerse(GetBibleVerseRequest request);
+        Task<BlobResponse?> GetBookOfTheBible(string bookName);
+        Task<ChaptersResponse?> GetChaptersInABookOfTheBible(string name);
+        Task<IEnumerable<BibleVerseResponse>> SearchBible(string query);
+    }
+}
