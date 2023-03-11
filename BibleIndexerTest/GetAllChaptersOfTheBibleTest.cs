171ca7510925f9d9c71a691fdfa25d6098ed7d79
Alexandra ogubuike <ogubuikealex@gmail.com>
2023-03-11 12:25:30 +0100

    test: get all verses in a chapter of the bible should return an empty list if chapter is invalid

--- BibleIndexerTest/GetAllChaptersOfTheBibleTest.cs
+++ BibleIndexerTest/GetAllChaptersOfTheBibleTest.cs
@@ -42,14 +42,40 @@ namespace BibleIndexerTest
             }
 
             [Fact]
-            public async Task GetAllVersesInAChapterOFTheBible_ShouldReturnNull_WhenInvalidChapterNumber()
+            public async Task GetAllVersesInAChapterOFTheBible_ShouldReturnEmptyList_WhenInvalidChapterNumber()
             {
                 // Arrange
+                const int invalidChapterNumber = 100;
+                const int verseNumber = 1;
+                const string bookName = "genesis";
+
+                var request = new GetBibleVerseRequest
+                {
+                    BookNameInFull = bookName,
+                    ChapterNumber = invalidChapterNumber,
+                    VerseNumber = verseNumber
+                };
+
+                // Act
+                var response = await BibleService.GetAllVersesInAChapterOFTheBible(request);
+
+                // Assert
+                Assert.Null(response.Resource);
+                Assert.IsAssignableFrom<IEnumerable<dynamic>>(response.DropDown);
+            }
+
+            [Fact]
+            public async Task GetAllVersesInAChapterOFTheBible_ShouldReturnNull_WhenInvalidBookName()
+            {
+                // Arrange
+                const int chapterNumber = 1;
+                const int verseNumber = 1;
+                const string invalidBookName = "InvalidBook";
                 var request = new GetBibleVerseRequest
                 {
                     BookNameInFull = "InvalidBook",
-                    ChapterNumber = 100,
-                    VerseNumber= 1
+                    ChapterNumber = chapterNumber,
+                    VerseNumber = verseNumber
                 };
 
                 // Act
@@ -71,10 +97,10 @@ namespace BibleIndexerTest
                 };
 
                 // Act
-                var response = await BibleService.GetAllVersesInAChapterOFTheBible(request);
+                VersesResponse response = await BibleService.GetAllVersesInAChapterOFTheBible(request);
 
                 // Assert
-                Assert.Null(response);
+                Assert.NotNull(response);
             }
         }
 
