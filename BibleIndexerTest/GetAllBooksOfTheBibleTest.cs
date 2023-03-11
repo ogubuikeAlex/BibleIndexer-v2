e2ae6cc77574c06bc1db4a141efb26b26eb3e31c
Alexandra ogubuike <ogubuikealex@gmail.com>
2023-03-11 11:44:40 +0100

    test: get all books of the bible should return 66 books

--- BibleIndexerTest/GetAllBooksOfTheBibleTest.cs
+++ BibleIndexerTest/GetAllBooksOfTheBibleTest.cs
@@ -1,4 +1,5 @@
-﻿using BibleIndexerV2.Services.Implementations;
+﻿using BibleIndexerV2.Models.Response;
+using BibleIndexerV2.Services.Implementations;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -14,23 +15,31 @@ namespace BibleIndexerTest
             public async Task GetAllBooksOfTheBible_ShouldReturnListOfBooks()
             {
                 // Arrange
-                var expectedBookCount = 66; // The Bible has 66 books
-
+                
                 // Act
                 object books = await BibleService.GetAllBooksOfTheBible();
 
                 // Assert
                 Assert.NotNull(books);
-                Assert.IsType<List<object>>(books);
-
-                var bookList = books as List<object>;
-                Assert.Equal(expectedBookCount, bookList.Count);
-
-                object firstBook = bookList.FirstOrDefault();
-                Assert.NotNull(firstBook);
-                Assert.True(firstBook.GetType().GetProperty("Id") != null);
-                Assert.True(firstBook.GetType().GetProperty("Book") != null);
+                Assert.IsType<List<object>>(books);                
             }
+
+            [Fact]
+            public async Task GetAllBooksOfTheBible_ShouldReturnSixtySixBooksOfTheBible()
+            {
+                // Arrange
+                var expectedBookCount = 66; // The Bible has 66 books
+
+                // Act
+                IEnumerable<GetAllBookResonse> bookList = await BibleService.GetAllBooksOfTheBible();               
+
+                //Assert
+                Assert.Equal(expectedBookCount, bookList.Count());                
+            }
+
+           
+
+
         }
     }
 }
